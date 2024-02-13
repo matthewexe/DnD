014dbda2b7bb4e0377eab672ed3c9b183dead156
matthewexe <matteobaldi233@gmail.com>
2024-02-13 20:50:18 +0100

    responses types modified

--- src/types/responses.ts
+++ src/types/responses.ts
@@ -1,20 +1,19 @@
 // Responses types of API https://5e-bits.github.io/docs/api/
 
 // Standard Types
-
-type APILevelReference = {
+export type APILevelReference = {
   index?: string;
   url?: string;
   level?: number;
 };
 
-type APINameReference = {
+export type APINameReference = {
   index?: string;
   name?: string;
   url?: string;
 };
 
-type APIReference = APILevelReference | APINameReference;
+export type APIReference = APILevelReference | APINameReference;
 
 type Choice = {
   desc?: string;
@@ -88,22 +87,48 @@ type Action = {
 };
 
 // Responses Types
+
+//****************** */
+// Common  Section
+//***************** */
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-all-resource-ur-ls
+ */
+export type AllResourceResponse = object;
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-list-of-all-available-resources-for-an-endpoint
+ */
 export type ResourceListResponse = {
   count: number;
   results: APIReference[];
 };
 
+//*********************** */
+// Character Data Section
+//*********************** */
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-an-ability-score-by-index
+ */
 export type AbilityScoreResponse = APIReference & {
   full_name?: string;
   desc?: string[];
   skills?: APIReference[];
 };
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-an-alignment-by-index
+ */
 export type AlignmentResponse = APIReference & {
   desc?: string;
   abbreviation?: string;
 };
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-background-by-index
+ */
 export type BackgroundResponse = APIReference & {
   starting_proficiencies?: APIReference[];
   starting_equipment?: APIReference[];
@@ -116,6 +141,9 @@ export type BackgroundResponse = APIReference & {
   flaws?: Choice[];
 };
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-language-by-index
+ */
 export type LanguageResponse = APIReference & {
   desc?: string;
   type?: string;
@@ -123,6 +151,9 @@ export type LanguageResponse = APIReference & {
   typical_speakers?: string[];
 };
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-proficiency-by-index
+ */
 export type ProficiencyResponse = APIReference & {
   type?: string;
   classes?: APIReference[];
@@ -130,11 +161,21 @@ export type ProficiencyResponse = APIReference & {
   reference?: APIReference;
 };
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-skill-by-index
+ */
 export type SkillResponse = APIReference & {
   desc?: string[];
   ability_score?: APIReference[];
 };
 
+//****************** */
+// Class Section
+//***************** */
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-class-by-index
+ */
 export type ClassResponse = APIReference & {
   hit_die?: number;
   class_levels?: string;
@@ -149,22 +190,92 @@ export type ClassResponse = APIReference & {
   subclasses?: APIReference[];
 };
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-spellcasting-info-for-a-class
+ */
+export type ClassSpellcastingResponse =
+  | {
+      level?: number;
+      info?: {name?: string; desc?: string[]};
+      spellcasting_ability?: APIReference[];
+    }
+  | {error: string};
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-multiclassing-resource-for-a-class
+ */
+export type ClassMultiCastingResponse = Multiclassing;
+
+//*************************** */
+// ClassResource List Section
+//*************************** */
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-subclasses-available-for-a-class
+ * @link https://5e-bits.github.io/docs/api/get-spells-available-for-a-class
+ * @link https://5e-bits.github.io/docs/api/get-features-available-for-a-class
+ * @link https://5e-bits.github.io/docs/api/get-proficiencies-available-for-a-class
+ */
 export type ClassResourceListResponse = ResourceListResponse;
 
-export type ClassLevelResponse =
-  | (APIReference & {
-      ability_score_bonuses?: number;
-      prof_bonus?: number;
-      features?: APIReference[];
-      spellcasting?: SpellCastingLevel;
-      class_specific?: object;
-    })
-  | ResourceListResponse;
+//*************************** */
+// Class Level Section
+//*************************** */
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-all-level-resources-for-a-class
+ * @link https://5e-bits.github.io/docs/api/get-level-resource-for-a-class-and-level
+ */
+export type ClassLevelResourceResponse = APIReference & {
+  ability_score_bonuses?: number;
+  prof_bonus?: number;
+  features?: APIReference[];
+  spellcasting?: SpellCastingLevel;
+  class_specific?: object;
+};
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-features-available-to-a-class-at-the-requested-level
+ */
+export type ClassLevelFeatureResponse = ResourceListResponse;
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-spells-of-the-requested-level-available-to-the-class
+ */
+export type ClassLevelSpellResponse = ResourceListResponse;
+
+//****************** */
+// Game Mechanics Section
+//***************** */
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-condition-by-index
+ * @link https://5e-bits.github.io/docs/api/get-a-damage-type-by-index
+ * @link https://5e-bits.github.io/docs/api/get-a-magic-school-by-index
+ */
 export type GameMechanicsResponse = APIReference & {
   desc?: string[] | string;
 };
 
+//****************** */
+// Equipment Section
+//***************** */
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-an-equipment-item-by-index
+ */
+export type EquipmentResponse = object;
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-an-equipment-category-by-index
+ */
+export type EquipmentCategory = APIReference & {
+  equipment?: APIReference[];
+};
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-magic-item-by-index
+ */
 export type MagicResponse = APIReference & {
   desc?: string[];
   equipment_category?: APIReference;
@@ -180,19 +291,30 @@ export type MagicResponse = APIReference & {
   variant?: APIReference;
 };
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-weapon-by-index
+ */
 export type WeaponResponse = APIReference;
 
-export type EquipmentResponse = object;
+//****************** */
+// Feat Section
+//***************** */
 
-export type EquipmentCategory = APIReference & {
-  equipment?: APIReference[];
-};
-
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-feat-by-index
+ */
 export type FeatResponse = APIReference & {
   desc?: string[];
   prerequisites?: Prerequisites;
 };
 
+//****************** */
+// Feature Section
+//***************** */
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-feature-by-index
+ */
 export type FeatureResponse = APIReference & {
   level?: number;
   desc?: string[];
@@ -203,8 +325,18 @@ export type FeatureResponse = APIReference & {
   feature_specific?: any;
 };
 
+//****************** */
+// Monster Section
+//***************** */
+
+/**
+ * @linkhttps://5e-bits.github.io/docs/api/get-list-of-monsters-with-optional-filtering
+ */
 export type MonsterResourceListResponse = ResourceListResponse;
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-monster-by-index
+ */
 export type MonsterResponse = APIReference & {
   desc?: string[];
   charisma?: number;
@@ -262,6 +394,13 @@ export type MonsterResponse = APIReference & {
   xp?: number;
 };
 
+//**************** */
+// Races Section
+//**************** */
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-race-by-index
+ */
 export type RaceResponse = APIReference & {
   speed?: number;
   ability_bonuses?: {bonus: number; ability_score: APIReference[]};
@@ -278,22 +417,51 @@ export type RaceResponse = APIReference & {
   subraces?: APIReference[];
 };
 
-export type SubracesByRaceResponse = ResourceListResponse;
+/**
+ * @link https://5e-bits.github.io/docs/api/get-subraces-available-for-a-race
+ */
+export type RaceSubracesResponse = ResourceListResponse;
 
-export type ProficiencyByRaceResponse = ResourceListResponse;
+/**
+ * @link https://5e-bits.github.io/docs/api/get-proficiencies-available-for-a-race
+ */
+export type RaceProficiencyResponse = ResourceListResponse;
 
-export type TraitsByRaceResponse = ResourceListResponse;
+/**
+ * @link https://5e-bits.github.io/docs/api/get-traits-available-for-a-race
+ */
+export type RaceTraitsResponse = ResourceListResponse;
 
+//**************** */
+// Rule Section
+//**************** */
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-rule-section-by-index
+ */
 export type RuleSectionResponse = APIReference & {
   desc?: string[];
 };
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-rule-by-index
+ */
 export type RuleResponse = RuleSectionResponse & {
   subsections?: RuleSectionResponse[];
 };
 
+//**************** */
+// Spells Section
+//**************** */
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-list-of-spells-with-optional-filtering
+ */
 export type SpellResourceListResponse = ResourceListResponse;
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-spell-by-index
+ */
 export type SpellResponse = APIReference & {
   desc?: string[];
   higher_level?: string[];
@@ -317,6 +485,13 @@ export type SpellResponse = APIReference & {
   url?: string;
 };
 
+//**************** */
+// Subclass Section
+//**************** */
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-subclass-by-index
+ */
 export type SubclassByIndexResponse = APIReference & {
   desc?: string;
   class?: APIReference;
@@ -325,14 +500,23 @@ export type SubclassByIndexResponse = APIReference & {
   spells?: {prerequisites: APIReference & {type: string}; spell: APIReference};
 };
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-features-available-for-a-subclass
+ */
 export type SubclassFeatureResponse = ResourceListResponse;
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-all-level-resources-for-a-subclass
+ */
 export type SubclassAllLevelResourceResponse = APIReference & {
   features?: APIReference[];
   class?: APIReference;
   subclass?: APIReference;
 };
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-level-resources-for-a-subclass-and-level
+ */
 export type SubclassLevelResourceResponse = APIReference & {
   ability_score_bonuses?: number;
   prof_bonus?: number;
@@ -341,8 +525,18 @@ export type SubclassLevelResourceResponse = APIReference & {
   classspecific?: any;
 };
 
-export type SubclassFeatureSpellLevelResposne = ResourceListResponse;
+/**
+ * @link https://5e-bits.github.io/docs/api/get-features-of-the-requested-spell-level-available-to-the-class
+ */
+export type SubclassFeatureSpellLevelResponse = ResourceListResponse;
 
+//**************** */
+// Subrace Section
+//**************** */
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-subrace-by-index
+ */
 export type SubraceIndexResponse = APIReference & {
   desc?: string;
   race?: APIReference;
@@ -353,10 +547,23 @@ export type SubraceIndexResponse = APIReference & {
   racial_traits?: APIReference[];
 };
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-proficiencies-available-for-a-subrace
+ */
 export type SubraceProficienciesResponse = ResourceListResponse;
 
+/**
+ * @link https://5e-bits.github.io/docs/api/get-traits-available-for-a-subrace
+ */
 export type SubraceTraitsResponse = ResourceListResponse;
 
+//**************** */
+// Trait Section
+//**************** */
+
+/**
+ * @link https://5e-bits.github.io/docs/api/get-a-trait-by-index
+ */
 export type TraitsIndex = APIReference & {
   desc?: string;
   races?: APIReference[];
