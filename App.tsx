2196b86976367e14cb3c6b18b62f6b01783c2aad
Sgrodo <mattiacappelletti00@gmail.com>
2024-02-17 19:04:25 +0100

    fetch non finito

--- App.tsx
+++ App.tsx
@@ -1,16 +1,14 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomeScreen } from './src/screens/HomeScreen';
-import { MyTabs } from './src/screens/BottomNavigator';
-import { MyButton } from './src/screens/MyButton';
-import { Counter } from './src/screens/Counter';
+import {View, Text, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {HomeScreen} from './src/screens/HomeScreen';
+import {MyTabs} from './src/screens/BottomNavigator';
+import {MyButton} from './src/screens/MyButton';
+import {Counter} from './src/screens/Counter';
 import ExportDwarf from './src/screens/prova';
-
-
 
 const Stack = createNativeStackNavigator();
 
@@ -19,9 +17,9 @@ function App() {
     <NavigationContainer>
       {/* <MyTabs /> */}
 
-      <HomeScreen/>
-      <MyButton title='Test'/>
-      <ExportDwarf>Aggiornamento</ExportDwarf>
+      <HomeScreen />
+      <MyButton title="Test" />
+      <ExportDwarf></ExportDwarf>
     </NavigationContainer>
   );
 }
--- src/screens/fetch.tsx
+++ src/screens/fetch.tsx
@@ -2,33 +2,31 @@ import {useEffect, useState} from 'react';
 import {Text} from 'react-native';
 import {useGetRacesByIndexQuery} from '../services/api';
 import {RaceIndexRequest} from '../types/requests';
-import {RaceResponse} from '../types/responses';
+import {Race} from '../types/responses';
 
-export default function ExportDwarf() {
-  const razze: RaceIndexRequest = [
-    'dragonborn',
-    'dwarf',
-    'elf',
-    'gnome',
-    'half-elf',
-    'half-orc',
-    'halfling',
-    'human',
-    'tiefling',
-  ];
+export default function ExportRace(input: RaceIndexRequest) {
+  // const [race, setRace] = useState<RaceIndexRequest>(input);
 
-  const [race, setRace] = useState<RaceIndexRequest>('dragonborn');
-
-  const {data, error, isLoading} = useGetRacesByIndexQuery({index: race});
+  const {data, error, isLoading, isFetching} = useGetRacesByIndexQuery({
+    index: input,
+  });
 
   if (error) return <Text>error in fetching</Text>;
   if (isLoading) return <Text>loading...</Text>;
   //se il result precedente non va bene
-
+  if (isFetching) <Text>attendi risposta dal server</Text>;
   return (
     <>
-      <Text>{data?.index ?? 'race not found'}</Text>
-      <Text>{data?.index ?? 'race not found'}</Text>
+      <Text>{data?.name ?? 'razza non disponibile'}</Text>
+      <Text>{data?.speed ?? 'velocità non disponibile'}</Text>
+      <Text>{data?.size ?? 'dimensione non disponibile'}</Text>
+      {data?.languages?.map((language, index) => (
+        <Text key={index}>{language.name}</Text>
+      ))}
+
+      {data?.traits?.map((traits, index) => (
+        <Text key={index}>{traits.name}</Text>
+      ))}
     </>
   );
 }
