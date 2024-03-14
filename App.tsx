607bcab6693707f2a98e73db1027deac1fde0678
matthewexe <matteobaldi233@gmail.com>
2024-03-14 18:05:53 +0100

    app.tsx adjusted

--- App.tsx
+++ App.tsx
@@ -1,24 +1,17 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import {View, Text, Button, SafeAreaView} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {SafeAreaView} from 'react-native';
 import {HomeScreen} from './src/screens/HomeScreen';
-import {MyTabs} from './src/screens/BottomNavigator';
 import {MyButton} from './src/screens/MyButton';
-import {Counter} from './src/screens/Counter';
 import RaceComponent from './src/components/race/fetchRaceByIndex';
-import {ApiProvider} from '@reduxjs/toolkit/query/react';
-
-const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <SafeAreaView>
       <HomeScreen />
       <MyButton title="Test" />
-      <RaceComponent input="dragonborn"></RaceComponent>
+      <RaceComponent input="dragonborn" />
     </SafeAreaView>
   );
 }
