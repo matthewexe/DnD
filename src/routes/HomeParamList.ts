d7efe14a0d53fbe5f964b033238e072c6087f259
matthewexe <matteobaldi233@gmail.com>
2024-03-19 16:03:41 +0100

    updated new ui

--- src/routes/HomeParamList.ts
+++ src/routes/HomeParamList.ts
@@ -1,10 +1,19 @@
 import {CompositeScreenProps} from '@react-navigation/native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootTabParamList, RootTabScreenProps} from './RootParamList';
+import {Player} from '../types/db';
 
 export type HomeStackParamList = {
   ListGame: undefined;
   GameDetail: {gameId: string};
+  NewGame: undefined;
+  SuccessError: {query: string};
+  GameDelete: {gameId: string};
+  NewPlayer_BasicInfo: {gameId: string};
+  NewPlayer_Race: Player;
+  NewPlayer_Class: Player;
+  NewPlayer_Equipment: Player;
+  NewPlayer_End: Player;
 };
 
 export type HomeStackScreenProps<T extends keyof HomeStackParamList> =
