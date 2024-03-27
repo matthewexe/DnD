f1815f40ea3dedc40353a06745427a66361ece69
Sgrodo <mattiacappelletti00@gmail.com>
2024-03-27 19:07:05 +0100

    fixed screen

--- src/screen/DictionaryScreen.tsx
+++ src/screen/DictionaryScreen.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {DictionaryStackParamList} from '../routes/DictionaryStack';
 import {HomeDictionary} from '../components/pages/HomeDictionary';
