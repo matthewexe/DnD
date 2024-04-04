625d9776ac452583e9a033aa818f6583d9ed42dc
matthewexe <matteobaldi233@gmail.com>
2024-04-04 14:06:24 +0200

    ability scores page workssssssssssss

--- src/components/pages/player/AbilityScoreLanding.tsx
+++ src/components/pages/player/AbilityScoreLanding.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {HomeScreenProps} from '../../../routes/HomeProps';
 import {NewPlayerView} from '../../../views/NewPlayerView';
 import {StyledButton} from '../../ui/StyledButton';
@@ -9,15 +9,20 @@ import {
   useGetSubRacesByIndexQuery,
 } from '../../../services/api';
 import {RacesRequest, SubraceIndexRequest} from '../../../types/requests';
+import {StyledTextInput} from '../../ui/StyledTextInput';
+import {InputText} from '../../ui/InputText';
+import {StyledSubtitle} from '../../ui/texts/StyledSubtitle';
+import {StyledText} from '../../ui/texts/StyledText';
 
 type Props = HomeScreenProps<'NewPlayer_AbilityScores'>;
 
 export const AbilityScoreLanding = ({navigation, route}: Props) => {
   const userData = useRef(route.params.playerData);
 
-  const [rolls, setRolls] = useState(
+  const [rolls, setRolls] = useState<number[]>(
     assegnaCaratteristiche(ordinaArrayNumeriCasuali(), userData.current.class),
   );
+  const [bonuses, setBonuses] = useState<number[]>(new Array(6).fill(0));
 
   const {
     data: raceData,
@@ -33,19 +38,96 @@ export const AbilityScoreLanding = ({navigation, route}: Props) => {
     index: userData.current.subrace as SubraceIndexRequest,
   });
 
-  const refRolls = useRef(rolls);
+  const labels = [
+    'Strength',
+    'Dexterity',
+    'Constitution',
+    'Intelligence',
+    'Wisdom',
+    'Charisma',
+  ];
+
+  const refRolls = useRef<number[]>(rolls);
   const refBonuses = useRef<number[]>(new Array(6).fill(0));
   const timeouts = useRef<NodeJS.Timeout[]>([]);
+  const labelBonuses = ['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA'];
 
-  const checkRoll = (roll: number) => {};
+  useEffect(() => {
+    refBonuses.current = new Array(6).fill(0);
+
+    raceData?.ability_bonuses.forEach(bonus => {
+      const idx = labelBonuses.indexOf(bonus.ability_score.name);
+      refBonuses.current[idx] += bonus.bonus;
+    });
+
+    subraceData?.ability_bonuses.forEach(bonus => {
+      const idx = labelBonuses.indexOf(bonus.ability_score.name);
+      refBonuses.current[idx] += bonus.bonus;
+    });
+
+    setBonuses([...refBonuses.current]);
+  }, [raceData, subraceData]);
 
   const onChangeText = (indexInputText: number, text: string) => {
     clearTimeout(timeouts.current[indexInputText]);
+
+    setBonuses(
+      refBonuses.current.map((bonus, index) =>
+        index === indexInputText ? 0 : bonus,
+      ),
+    );
+
+    const value = parseInt(text, 10);
+    setRolls(
+      rolls.map((roll, index) => (index === indexInputText ? value : roll)),
+    );
+    refRolls.current[indexInputText] = isNaN(value) ? 10 : Math.min(value, 28);
+    timeouts.current[indexInputText] = setTimeout(() => {
+      setBonuses([...refBonuses.current]);
+
+      setRolls([...refRolls.current]);
+    }, 1000);
+  };
+
+  const reroll = () => {
+    refRolls.current = assegnaCaratteristiche(
+      ordinaArrayNumeriCasuali(),
+      userData.current.class,
+    );
+
+    setRolls([...refRolls.current]);
+
+    console.log(refRolls.current);
   };
 
   return (
     <NewPlayerView title="Ability Scores" errorOnPress={() => {}}>
-      <StyledButton text="reroll" />
+      <StyledButton text="Re-Roll" onPress={reroll} />
+      {rolls.map((roll, index) => (
+        <InputText
+          label={labels[index]}
+          keyboardType="number-pad"
+          placeholder="10 + bonus"
+          value={
+            isNaN(roll + bonuses[index])
+              ? ''
+              : (roll + bonuses[index]).toString()
+          }
+          onChangeText={text => onChangeText(index, text)}
+        />
+      ))}
+      <StyledSubtitle>Race Bonuses</StyledSubtitle>
+      {raceData?.ability_bonuses.map(bonus => (
+        <StyledText>
+          {bonus.ability_score.name}: {bonus.bonus}
+        </StyledText>
+      ))}
+      <StyledSubtitle>Subrace Bonuses</StyledSubtitle>
+      {subraceData?.ability_bonuses.map(bonus => (
+        <StyledText>
+          {bonus.ability_score.name}: {bonus.bonus}
+        </StyledText>
+      ))}
     </NewPlayerView>
   );
 };
